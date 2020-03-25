bec763d0958570a6721b62c65969b00f4882d6ab
Jens Tandstad <jens.tandstad@gmail.com>
2020-03-25 13:22:58 +0100

    Adding TheScience

---
+++ src/Coprone/Web/Pages/TheScience.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Pages
+{
+    public class TheScienceModel : PageModel
+    {
+        private readonly ILogger<TheScienceModel> _logger;
+
+        public TheScienceModel(ILogger<TheScienceModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
