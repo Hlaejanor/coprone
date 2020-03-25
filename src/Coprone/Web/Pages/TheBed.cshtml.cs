c018593097fff27f71ea48fe4d2b0d48b1bc0bfe
Jens Tandstad <jens.tandstad@gmail.com>
2020-03-25 15:15:13 +0100

    Adding page

---
+++ src/Coprone/Web/Pages/TheBed.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Pages
+{
+    public class TheBedModel : PageModel
+    {
+        private readonly ILogger<TheBedModel> _logger;
+
+        public TheBedModel(ILogger<TheBedModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
