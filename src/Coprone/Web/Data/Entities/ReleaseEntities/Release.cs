bbb3b04d8864f822773ada91fe0a22e1155c5c1e
Bjørn Tore Gjerde <btgjerde@gmail.com>
2020-03-25 18:57:14 +0100

    Add missing file that was ignored by gitignore because of naming

---
+++ src/Coprone/Web/Data/Entities/ReleaseEntities/Release.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Data
+{
+    public class Release
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Version { get; set; }
+
+        [MaxLength(255)]
+        public string Contributors { get; set; }
+
+        public string Licence { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTime CreatedUtc { get; set; }
+    }
+}
