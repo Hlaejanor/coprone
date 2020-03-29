8a60da6e25993ddfb13396be7ed2b8a0cbb1ff18
herreovertidogrom <jens.tandstad@gmail.com>
2020-03-29 13:30:54 +0200

    Adding

--- src/Coprone/Web/Pages/TheBed.cshtml.cs
+++ src/Coprone/Web/Pages/TheBed.cshtml.cs
@@ -4,21 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Web.Data;
 
 namespace Web.Pages
 {
     public class TheBedModel : PageModel
     {
         private readonly ILogger<TheBedModel> _logger;
-
-        public TheBedModel(ILogger<TheBedModel> logger)
+        private readonly Web.Data.ApplicationDbContext _context;
+        public TheBedModel(ILogger<TheBedModel> logger, Web.Data.ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+        public IList<Release> Release { get; set; }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            Release = await _context.Release.ToListAsync();
         }
+      
     }
 }
